6b8fa8e6c196db53dd88c8b68e6108b9fc07ac54
Sananikone <lilian.sananikone@laposte.net>
2021-04-07 13:58:39 +0200

    UI : display list of players for one room adding

---
+++ Digup/Assets/Scripts/UI/Rooms/PlayerListing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerListing : MonoBehaviour
+{
+    [SerializeField]
+    private Text _text;
+
+    public Player Player { get; private set; }
+
+    public void SetPlayerInfo(Player Player)
+    {
+        this.Player = Player;
+        _text.text = Player.NickName;
+    }
+}
---
+++ Digup/Assets/Scripts/UI/Rooms/PlayerListingsDisplay.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerListingsDisplay : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Transform _content;
+    [SerializeField]
+    private PlayerListing _playerListing;
+
+    private List<PlayerListing> _listings = new List<PlayerListing>();
+
+    private void AddPlayerListing(Player NewPlayer)
+    {
+        PlayerListing Listing = Instantiate(_playerListing, _content);
+        if (Listing != null)
+        {
+            Listing.SetPlayerInfo(NewPlayer);
+            _listings.Add(Listing);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player NewPlayer)
+    {
+        //AddPlayerListing(NewPlayer);
+        PlayerListing Listing = Instantiate(_playerListing, _content);
+        if (Listing != null)
+        {
+            Listing.SetPlayerInfo(NewPlayer);
+            _listings.Add(Listing);
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player OtherPlayer)
+    {
+        int Index = _listings.FindIndex(x => x.Player == OtherPlayer);
+        if (Index != -1)
+        {
+            Destroy(_listings[Index].gameObject);
+            _listings.RemoveAt(Index);
+        }
+    }
+
+    private void GetCurrentRoomPlayers()
+    {
+        foreach (KeyValuePair<int, Player> PlayerInfo in PhotonNetwork.CurrentRoom.Players)
+        {
+            AddPlayerListing(PlayerInfo.Value);
+        }
+    }
+}
