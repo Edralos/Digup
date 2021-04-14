62a9ca92f99976937f4346c680e98ebded16c8d9
Quentin <quentin.robard@beecoming.fr>
2021-04-14 23:55:34 +0200

    Preparation generation de la pool et link des rooms

--- Digup/Assets/Scripts/Map/Stage.cs
+++ Digup/Assets/Scripts/Map/Stage.cs
@@ -1,56 +1,96 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Stage : MonoBehaviour
 {
-    private string name;
-    private StartRoom startRoom;
+    private string Name;
+    private StartRoom StartRoom;
 
-    public void initStage()
+    public Stage(string Name, int NbRooms)
     {
-        /* ---------------------------- */
-        /* Randomly generate the stages */
-        /* ---------------------------- */
-
-        int Depth = 0;
-
-        //Add the default 1st Room
-        startRoom = new StartRoom(Depth);
-        Depth++;
-
-        //Add the 2 following CombatRoom
-        startRoom.addNextRoom(new CombatRoom(Depth));
-        startRoom.addNextRoom(new CombatRoom(Depth));
-        Depth++;
-
-        //Randomly add the 10 next depth rooms
-        
-
-        //Add 2 break rooms
-
-
-        //Add the boss room
-
+        this.Name = Name;
+        InitStage(NbRooms, 3);
     }
 
-    private string toString()
+    public void InitStage(int MaxDepth, int Width)
     {
-        string sRet = "";
+        /*-----------------------------------------------*/
+        /*         Randomly generate the stages          */
+        /*-----------------------------------------------*/
+        List<StageRoom> CurrentRooms = new List<StageRoom>();
+        List<StageRoom> NextRooms = new List<StageRoom>();
+        List<StageRoom> Pool = new List<StageRoom>();
 
-        StageRoom currentRoom = startRoom;
+        /*-------------------------------*/
+        /*   Fill the Pool with Rooms    */
+        /*-------------------------------*/
+        int MaxRoomsInPool = (MaxDepth - 4) * Width;
+        //50% of Combat rooms
+
+        //25% of Treasure or Trap rooms
+
+        //25% of Other rooms
+
+
+
+        for (int Depth = 0; Depth< MaxDepth; Depth++)
+        {
+            if(Depth == 0) {
+                this.StartRoom = new StartRoom(Depth);
+                CurrentRooms.Add(this.StartRoom);
+
+                NextRooms.Add(new CombatRoom(Depth));
+                NextRooms.Add(new CombatRoom(Depth));
+            } else if(Depth == MaxDepth - 1)
+            {
+                NextRooms.Add(new TreasureRoom(Depth));
+                NextRooms.Add(new ShopRoom(Depth));
+            } else if(Depth == MaxDepth) {
+                NextRooms.Add(new BossRoom(Depth));
+            } else {
+                //Pick 3 random Room from the Pool
+
+            }
+
+            //For each current rooms
+            foreach(StageRoom CurrentRoom in CurrentRooms) {
+                //Create a Array copy of the NextRooms
+                List<StageRoom> NextRoomsCopy = new List<StageRoom>(NextRooms);
+                Debug.Log(NextRoomsCopy);
+
+                //Link the current Room to the next
+                for (int i = 0; i < NextRoomsCopy.Count && i<2; i++) {
+                    //Pick a random Room 
+                    StageRoom RandomNextRoom = NextRoomsCopy[new System.Random().Next(0,NextRoomsCopy.Count)];
+                    //Add it to the next Rooms
+                    CurrentRoom.addNextRoom(RandomNextRoom);
+                    //Delete it from the list
+                    NextRoomsCopy.RemoveAt(NextRoomsCopy.IndexOf(RandomNextRoom));
+                }
+            }
+
+            CurrentRooms = new List<StageRoom>(NextRooms);
+            NextRooms = new List<StageRoom>();
+        }
+    }
+
+    private string ToString()
+    {
+        string StringRet = "";
+
+        StageRoom currentRoom = StartRoom;
         while(currentRoom.getNextRooms.Count != 0)
         {
             Debug.Log("yo");
         }
 
-        return sRet;
+        return StringRet;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        toString();
+        ToString();
     }
 
     // Update is called once per frame
@@ -58,4 +98,5 @@ public class Stage : MonoBehaviour
     {
         
     }
+
 }
