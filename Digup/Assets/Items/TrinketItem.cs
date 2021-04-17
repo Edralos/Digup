ecede376fd4a1bc11e8f07b684b0dadf4a9325ed
Edralos <titimornet@gmail.com>
2021-04-17 19:22:54 +0200

    Game data terminé, à tester + Items et Characters avecinitialisation des Json

--- Digup/Assets/Items/TrinketItem.cs
+++ Digup/Assets/Items/TrinketItem.cs
@@ -1,11 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class TrinketItem : EquipmentItem
 {
-
-    public TrinketItem(string name, string desc, string flavor) : base(name, desc, flavor)
+    public TrinketItem(string name, string desc, string flavor, EquipmentType type, Effect effect) : base(name, desc, flavor, type, effect)
     {
     }
 
