f9e2dd4f2dfea22e3ce380e348aa059a009ed4d2
Sananikone <lilian.sananikone@laposte.net>
2021-04-21 16:06:23 +0200

    script-UI : changing player's icon color when another player leaves the room

--- Digup/Assets/Scripts/UI/Rooms/PlayerListing.cs
+++ Digup/Assets/Scripts/UI/Rooms/PlayerListing.cs
@@ -13,10 +13,12 @@ public class PlayerListing : MonoBehaviour, IPunObservable
     private GameObject _readyIcon;
 
     public Player Player { get; private set; }
+    public int ID { get; set; }
 
-    public void SetPlayerInfo(Player Player)
+    public void SetPlayerInfo(Player Player, int ID)
     {
         this.Player = Player;
+        this.ID = ID;
         _text.text = Player.NickName;
         _readyIcon.transform.localScale = Vector3.zero;
     }
