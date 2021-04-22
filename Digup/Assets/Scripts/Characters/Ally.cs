d0a2231543b3e4e4c24a105a29663111ada0646c
Edralos <titimornet@gmail.com>
2021-04-23 01:03:06 +0200

    Update Ally.cs and ActiveItem.cs. Commentaire code + ajout méthode AddEquipment

--- Digup/Assets/Scripts/Characters/Ally.cs
+++ Digup/Assets/Scripts/Characters/Ally.cs
@@ -8,8 +8,41 @@ namespace Assets.Characters
 {
     public class Ally : Character
     {
-        public Ally(string name, int maxHP, int prot, int dodge, int atkMods, int acc, int speed) : base(name, maxHP, prot, dodge, atkMods, acc, speed)
+        public List<ConsumableItem> ConsumableBelt { get; protected set; } = new List<ConsumableItem>();
+        public List<EquipmentItem> Equipment { get; protected set; } = new List<EquipmentItem>();
+        public int MaxEquipmentSlots;
+        public int UnlockedSlots;
+        public int BeltSize;
+        public Ally(string name, int maxHP, int prot, int dodge, int atkMods, int acc, int speed, int maxSlots, int unlockedSlots, int beltSize) : base(name, maxHP, prot, dodge, atkMods, acc, speed)
         {
+            MaxEquipmentSlots = maxSlots;
+            UnlockedSlots = unlockedSlots;
+            BeltSize = beltSize;
+        }
+
+
+        public bool AddEquipment(EquipmentItem equipment)
+        {
+            if (equipment is ActiveItem active)
+            {
+                if (Equipment.Where( a => a.EquipmentType == active.EquipmentType && a.EquipmentType != EquipmentType.NULL).Count() > 0)
+                {
+                    return false;
+                }
+                
+                    Equipment.Add(equipment);
+                    return true;
+                
+            }
+            else if (equipment is TrinketItem trinket)
+            {
+                if (Equipment.Where(a => a.EquipmentType == trinket.EquipmentType && a.EquipmentType != EquipmentType.NULL).Count() > 0)
+                {
+                    return false;
+                }
+                return false;
+            }
+            return false;
         }
     }
 }
--- Digup/Assets/Scripts/Items/ActiveItem.cs
+++ Digup/Assets/Scripts/Items/ActiveItem.cs
@@ -3,6 +3,15 @@
 /// </summary>
 public class ActiveItem : EquipmentItem
 {
+    /// <summary>
+    /// ActiveItem constructor
+    /// </summary>
+    /// <param name="name">Name</param>
+    /// <param name="desc">Description</param>
+    /// <param name="flavor">Flavor text</param>
+    /// <param name="effect">Effect</param>
+    /// <param name="rarity">Rarity</param>
+    /// <param name="type">Equipment type. default : NULL</param>
     public ActiveItem(string name, string desc, string flavor, Effect effect, Rarity rarity, EquipmentType type = EquipmentType.NULL) : base(name, desc, flavor, effect, rarity,type)
     {
     }
