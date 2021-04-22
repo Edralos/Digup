515437420a795e34159d32b00e54b9f30cb54c8d
Edralos <titimornet@gmail.com>
2021-04-22 22:41:06 +0200

    réorganisation fichiers coté THibaud

---
+++ Digup/Assets/Scripts/Items/ActiveItem.cs
@@ -0,0 +1,7 @@
+public class ActiveItem : EquipmentItem
+{
+    // Start is called before the first frame update
+    public ActiveItem(string name, string desc, string flavor, EquipmentType type, Effect effect) : base(name, desc, flavor, type, effect)
+    {
+    }
+}
---
+++ Digup/Assets/Scripts/Items/ConsumableItem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ConsumableItem : Item
+{
+    public Effect Effect;   
+
+    public ConsumableItem(string name, string desc, string flavor ,Effect effect) : base(name, desc, flavor)
+    {
+        Effect = effect;
+    }
+
+
+}
