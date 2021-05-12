b8157fcfa31514441ff27e39960ea3bda43e6bf2
Edralos <titimornet@gmail.com>
2021-05-12 15:10:14 +0200

    rebased

---
+++ Digup/Assets/Scripts/Game/PartyData.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Characters;
+using System.Collections.Generic;
+
+public static class PartyData
+{
+    public static Dictionary<int, Ally> Allies { get; private set; }
+
+    public static Ally RemoveAlly(int id)
+    {
+        Ally character = Allies[id];
+        Allies.Remove(id);
+        return character;
+    }
+
+    public static void AddPlayer(int id, Ally character)
+    {
+        Allies.Add(id, character);
+    }
+    
+}
